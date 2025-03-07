e95b3b92c61de7c45e3e4d55b79c84000b5c7952
Tahmim Hasan Sany <sanytahmim@gmail.com>
2025-03-07 07:08:05 +0600

    removed unused files

--- others/Od/script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function () {
-    const nav = document.querySelector('nav');
-    const navLinks = document.querySelector('.nav-links');
-
-    nav.addEventListener('click', function () {
-        navLinks.classList.toggle('active');
-    });
-});
